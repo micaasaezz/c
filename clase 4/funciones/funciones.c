39e88986607210bc45827e4d5d0d4db0e22e6170
Micaela Saez <mica_saez@hotmail.com>
2017-03-28 10:24:55 -0300

    ejercicio funciones

---
+++ clase 4/funciones/funciones.c
@@ -0,0 +1,28 @@
+
+int sumar(int a,int b)
+{
+    int resultado;
+    resultado=a+b;
+    return resultado;
+}
+
+int restar(int a, int b)
+{
+    int resultado;
+    resultado=a-b;
+    return resultado;
+}
+
+int multiplicar(int a,int b)
+{
+    int resultado;
+    resultado=a*b;
+    return resultado;
+}
+
+float dividir(int a,int b)
+{
+    float resultado;
+    resultado = a/b;
+    return resultado;
+}
