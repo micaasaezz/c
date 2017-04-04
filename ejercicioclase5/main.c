f0edfb77bd0878e1a6bce75123c0b77f0ada3c9e
Micaela Saez <mica_saez@hotmail.com>
2017-04-04 12:13:49 -0300

    Add files via upload

---
+++ ejercicioclase5/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    char nombre[30], apellido[30], nombreApellido[60];
+    int i;
+
+    printf("Ingrese su nombre: ");
+    gets(nombre);
+
+    printf("Ingrese su apellido: ");
+    gets(apellido);
+
+
+//    nombre[0] = toupper(nombre[0]);
+//    apellido[0] = toupper(apellido[0]);
+
+    strcat(nombre, " ");
+    strcat(nombreApellido, nombre);
+    strcat(nombreApellido, apellido);
+
+    nombreApellido[0] = toupper(nombreApellido[0]);
+
+    for(i=0; i<strlen(nombreApellido); i++)
+    {
+        if(nombreApellido[i]==' ')
+        {
+            nombreApellido[i+1] = toupper(nombreApellido[i+1]);
+        }
+    }
+
+
+    puts(nombreApellido);
+
+    printf("                         ,;;;, ,;;;,\n");
+    printf("                       ;;;;;;,;;;;;;;\n");
+    printf("               .:::. .::::;;;;;;;;;;;\n");
+    printf("             :::::::.:::::::;;;;;;;;;'\n");
+    printf("            :::::::::::::::;;;;;;;'\n");
+    printf("            ':::::::::::::';;;;;'\n");
+    printf("              ':::::::::' ';'\n");
+    printf("                 ':::::'\n");
+    printf("                   ':'\n");
+
+    return 0;
+}
