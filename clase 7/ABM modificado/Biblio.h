1ebce9998e24597246d0a01bf9ed6ba68cfb206d
Micaela Saez <mica_saez@hotmail.com>
2017-04-10 21:15:01 -0300

    Add files via upload

---
+++ clase 7/ABM modificado/Biblio.h
@@ -0,0 +1,10 @@
+
+void mostrarDatos(int[], char[][15], int[],  int);
+
+void cargarDatos(int[], char[][15], int[],  int);
+
+void bajaDatos(int[], char[][15], int[],  int);
+
+void modificarDatos(int[], char[][15], int[],  int);
+
+void ordenarPorNombre(int[], char[][15], int[],  int);
