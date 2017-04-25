80bbee5412694e281779c9e15eaf767914b74d10
Micaela Saez <mica_saez@hotmail.com>
2017-04-25 09:57:37 -0300

    Add files via upload

---
+++ clase 25 abril/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+/*
+2- mostrar cada uno
+3- mostrar clientes con su serie
+4- por cada serie los clientes que la estan viendo
+5- todos los clientes que ven TBBT (100)
+6- La serie menos popular
+
+Crear una cuarta estructura que permita relacionar un cliente con una serie, de esta manera podremos obtener una relacion
+muchos a muchos (Un cliente que ve muchas series)
+
+7- Todas las series que ven los clientes de nombre "Juan"
+*/
+
+
+int main()
+{
+    eSerie serie[5];
+    cargarSeries(serie);
+
+    eContador cont[5];
+
+    eClienteSerie clienteSerie[10];
+
+
+    eCliente cliente[10];
+    cargarClientes(cliente);
+
+    iniciareClienteSerie(serie, cliente, 10, clienteSerie);
+
+    mostrarSerie(serie, 5);
+    printf("\n____________________________________________\n");
+    mostrarCliente(cliente, serie, 5, 10);
+
+    printf("\n____________________________________________\n");
+    mostarSeriesConClientes(serie, cliente, 5, 10);
+
+    int contTBBT = clientesVenTBBT(serie, 5);
+
+
+    return 0;
+}
